3b8bda3e3194c8c80159b6119235c3b29a0b5aed
phamleduy1983 <phamleduy1983@gmail.com>
2017-03-16 22:10:36 +0700

    update

--- ArduinoService/ArduinoService/obj/Release/Package/PackageTmp/Scripts/View/Garden.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('.dashboard-div-wrapper').click(function () {
-        var isActive = $(this).parent().attr('isactive');
-        if (isActive == 1)
-            window.location = $(this).attr('url');
-        else
-            toastr.warning("Khu vườn chưa kích hoạt !");
-    });
-});
